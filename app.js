a380fe8ee1927071fb504c261d4a61eca38b978e
Devendra Pratap Singh <59687571+devendradbdigital@users.noreply.github.com>
2022-04-20 10:02:36 +0530

    file-update

--- app.js
+++ app.js
@@ -1,63 +1,44 @@
-const fs = require("fs");
 const express = require("express");
-const req = require("express/lib/request");
 const app = express();
+const fs = require("fs");
 app.use(express.json());
+const PORT = 8000;
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/data/users.json`));
-
-// app.get("/", (req, res) => {
-//   res.status("200").json({ message: "Hello from the express" });
-// });
-
-// app.post("/", (req, res) => {
-//   res.end("Express endpoint...");
-// });
 
 app.get("/api/v1/users", (req, res) => {
   res.status(200).json({
     status: "success",
-    result: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
-app.get("/api/v1/users/:id", (req, res) => {
-  const id = req.params.id * 1;
-  const userId = users.find((el) => el.id === id);
-  //   if (id > users.length) {
-  if (!userId) {
-    return res.status(404).json({
-      status: "Failed",
-      message: `Id ${id} is not found`,
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      userId,
-    },
+    results: users.length,
+    message: "Hello form the other side...",
+    data: { users },
   });
 });
 
 app.post("/api/v1/users", (req, res) => {
-  const newID = users[users.length - 1].id + 1;
-  const newUser = Object.assign({ id: newID }, req.body);
+  const userId = users[users.length - 1].id + 1;
+  const newUser = Object.assign({ id: userId }, req.body);
   users.push(newUser);
-  fs.writeFile(`${__dirname}/data/users.json`, JSON.stringify(users), (err) => {
+  fs.writeFile(`${__dirname}/data/users/json`, JSON.stringify(users), (err) => {
     res.status(201).json({
-      status: "success",
+      status: "succes",
+      message: "New User Added...",
       data: {
-        users: newUser,
+        user: newUser,
       },
     });
   });
 });
 
-const port = 9000 || "";
-app.listen(port, () => {
-  console.log(`Listening from the port ${port}...`);
+app.get("/api/v1/users/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const userData = users.find((item) => item.id === id);
+  res.status(200).json({
+    message: "Success",
+    data: {
+      userData,
+    },
+  });
 });
+
+app.listen(PORT);
